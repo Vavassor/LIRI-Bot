986b8c4f9fbd7a1b1340dffb2c8228dbdd8a8f58
Andrew Dawson <dawso.andrew@gmail.com>
2019-01-26 19:59:39 -0500

    Adds command-line parsing and makes spotify-this-song command functional.

--- liri.js
+++ liri.js
@@ -2,18 +2,68 @@ const Spotify = require("node-spotify-api");
 require("dotenv").config();
 const keys = require("./keys.js");
 
-const spotify = new Spotify(keys.spotify);
+function outputResult(result) {
+  console.log(result);
+}
 
-spotify.search({
-  type: "track",
-  query: "I Want it That Way",
-}).then((response) => {
-  const firstResult = response.tracks.items[0];
-  const artistNames = firstResult.artists.map(artist => artist.name);
+function concertThis(args) {
+  console.log("Not implemented yet!");
+}
 
-  console.log(
-      "Track Name: " + firstResult.name + "\n"
-      + "Artists: " + artistNames.join(", ") + "\n"
-      + "Album: " + firstResult.album.name + "\n"
-      + "Preview URL: " + firstResult.preview_url);
-});
+function doWhatItSays() {
+  console.log("Not implemented yet!");
+}
+
+function executeCommand(args) {
+  if(args.length < 1) {
+    console.error("Please enter a command.");
+  } else {
+    switch (args[0]) {
+      case "concert-this":
+        concertThis(args);
+        break;
+
+      case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+      case "movie-this":
+        movieThis(args);
+        break;
+
+      case "spotify-this-song":
+        spotifyThisSong(args);
+        break;
+    }
+  }
+}
+
+function movieThis(args) {
+  console.log("Not implemented yet!");
+}
+
+function spotifyThisSong(args) {
+  if (args.length !== 2) {
+    console.error("Please enter one song to search for on spotify.");
+    return;
+  }
+
+  const songName = args[1];
+  const spotify = new Spotify(keys.spotify);
+
+  spotify.search({
+    type: "track",
+    query: songName,
+  }).then((response) => {
+    const firstResult = response.tracks.items[0];
+    const artistNames = firstResult.artists.map(artist => artist.name);
+  
+    outputResult("Track Name: " + firstResult.name + "\n"
+        + "Artists: " + artistNames.join(", ") + "\n"
+        + "Album: " + firstResult.album.name + "\n"
+        + "Preview URL: " + firstResult.preview_url);
+  });
+}
+
+const nodeArgs = process.argv.slice(2);
+executeCommand(nodeArgs);
